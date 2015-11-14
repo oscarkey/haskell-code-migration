fac9233ce1aef8c6efc917a05387beffdd0b1ec2
Oscar Key <oscar@keyfamily.org.uk>
2015-11-14 00:42:25 +0000

    Converted tabs to spaces.

--- Migration.hs
+++ Migration.hs
@@ -17,28 +17,28 @@ data CompTree a = MigrateEffect (CompTree a) | Result a deriving (Show,Read)
 [operation|Migrate :: ()|]
 
 type MigrationComp a =
-	([handles|h {Migrate}|], Num a) => Comp h a
+    ([handles|h {Migrate}|], Num a) => Comp h a
 
 [handler|
-	ReifyComp a :: CompTree a
-		handles {Migrate} where
-			Return x -> Result x
-			Migrate k -> reifyComp (MigrateEffect (k ()))
+    ReifyComp a :: CompTree a
+        handles {Migrate} where
+            Return x -> Result x
+            Migrate k -> reifyComp (MigrateEffect (k ()))
 |]
 
 [shallowHandler|
-	RunMigration a :: a
-		handles {Migrate} where
-			Return x -> x
-			Migrate k -> reifyComp (k ())
+    RunMigration a :: a
+        handles {Migrate} where
+            Return x -> x
+            Migrate k -> reifyComp (k ())
 |]
 
 testComp :: MigrationComp String
 testComp = do {
-	migrate;
-	return "howdy"
+    migrate;
+    return "howdy"
 }
 
 main = do 
-	runMigration testComp
-	return ()
+    runMigration testComp
+    return ()
