15024ae6f6511bbe010b8729700a0d1ded98991c
Oscar Key <oscar@keyfamily.org.uk>
2016-01-03 22:23:07 +0000

    Added abstract lists, restricted to [Char] at the moment.

--- Migration.hs
+++ Migration.hs
@@ -7,7 +7,10 @@
     FlexibleContexts,
     OverlappingInstances,
     UndecidableInstances,
-    ConstraintKinds #-}
+    ConstraintKinds,
+    OverloadedLists,
+    OverloadedStrings,
+    StandaloneDeriving #-}
 
 module Migration where
 
@@ -16,6 +19,9 @@ import DesugarHandlers
 import Network.Simple.TCP (connect, listen, accept, HostPreference(Host), HostName)
 import Network.Socket (recv, send)
 import qualified Data.Map.Strict as Map
+import Data.List
+import Data.String
+import GHC.Exts (IsList(Item, fromList, toList))
 
 portNum = "8000"
 
@@ -23,6 +29,7 @@ portNum = "8000"
 -- Store.
 data StoreKey a = StoreKey Int deriving (Show, Read)
 data StoreValue = StoreIntValue Int 
+                | StoreCharListValue [Char] 
     deriving (Show, Read)
 type Store = Map.Map Int StoreValue
 
@@ -47,6 +54,13 @@ instance Storeable Int where
     retrieve store k = 
         let v = generalRetrieve store k
         in case v of StoreIntValue x -> x
+                     _ -> error "Wrong type in store"
+
+instance Storeable [Char] where
+    save store k x = generalSave store k (StoreCharListValue x)
+    retrieve store k = 
+        let v = generalRetrieve store k
+        in case v of StoreCharListValue x -> x
                      _ -> error "Wrong type in store"
 
 
@@ -80,11 +94,57 @@ evalAbsInt store (OpMult  x y) = (evalAbsInt store x) * (evalAbsInt store y)
 evalAbsInt store (OpSig     x) = signum $ evalAbsInt store x
 
 
+-- Abstract lists.
+data AbsList a = Nil
+               | ListVar (StoreKey [a])
+               | Cons a (AbsList a)
+               | Append (AbsList a) (AbsList a)
+               | Take Int (AbsList a)
+               | Drop Int (AbsList a)
+deriving instance Show a => Show (AbsList a)
+deriving instance Read a => Read (AbsList a)
+
+type AbsString = AbsList Char
+
+instance IsList (AbsList a) where
+    type Item (AbsList a) = a
+    fromList     [] = Nil
+    fromList (x:xs) = Cons x (fromList xs)
+    toList xs = error "Cannot convert from AbsList to [] without store."
+
+instance IsString (AbsList Char) where
+    fromString s = fromList s
+
+evalAbsList :: (Storeable [a]) => Store -> AbsList a -> [a]
+evalAbsList store          Nil = []
+evalAbsList store (ListVar  k) = retrieve store k
+evalAbsList store (Cons  x xs) = x : (evalAbsList store xs)
+evalAbsList store (Append x y) = (evalAbsList store x) ++ (evalAbsList store y)
+evalAbsList store (Take  n xs) = take n (evalAbsList store xs)
+evalAbsList store (Drop  n xs) = drop n (evalAbsList store xs)
+
+acons :: a -> AbsList a -> AbsList a
+acons x xs = Cons x xs
+
+(+++) :: AbsList a -> AbsList a -> AbsList a
+(+++) x y = Append x y
+
+atake :: Int -> AbsList a -> AbsList a
+atake n xs = Take n xs
+
+adrop :: Int -> AbsList a -> AbsList a
+adrop n xs = Drop n xs
+
+
+-- Abstract Show.
 class AbsShow a where
     ashow :: Store -> a -> String
 
 instance AbsShow AbsInt where
     ashow store x = show $ evalAbsInt store x
+
+instance (Storeable [a], Show a) => AbsShow (AbsList a) where
+    ashow store x = show $ evalAbsList store x
 
 instance AbsShow Int where
     ashow store x = show x
