555c24edae39fff68530ff63dcec3d97e1036dc3
Oscar Key <oscar@keyfamily.org.uk>
2016-01-19 20:57:29 +0000

    Allowed the user to enter a class name and a list of 5 names.
    Currently this is unable to print them out at the receiver, additional
    functionality is required in the library.

--- MigratingRegister.hs
+++ MigratingRegister.hs
@@ -5,14 +5,24 @@
 
 import Migration
 
+readNames :: MigrationComp (AbsList AbsString)
+readNames =
+    let limited 0 = return Nil 
+        limited x = do
+            name <- readStr
+            rest <- limited (x-1)
+            return $ acons name rest
+    in limited 5
+
 registerComp :: MigrationComp ()
-registerComp = do 
-    printStr "How many pupils are currently present?"
-    count <- readStr
-    printStr "And what is your name?"
-    name <- readStr
+registerComp = do
+    printStr "Which class is this?"
+    className <- readStr
+    printStr "Enter the present student names: (max 5)"
+    names <- readNames
     migrate "127.0.0.1"
-    printStr $ name +++ " says that the number of pupils present was " +++ count
+    printStr $ "The people present in class " +++ className +++ " are:"
+    -- Print the list of people present
     return ()
 
 main :: IO ()
