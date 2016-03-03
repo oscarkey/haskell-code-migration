d8d822af1c74790c0833e05a4d29299f76b3c96c
Oscar Key <oscar@keyfamily.org.uk>
2016-03-03 18:02:43 +0000

    Updated version of the migrating register (forgot to commit).

--- MigratingRegister.hs
+++ MigratingRegister.hs
@@ -2,33 +2,44 @@
     FlexibleContexts,
     ConstraintKinds,
     OverloadedStrings,
+    OverloadedLists,
     RankNTypes #-}
 
 import Migration
+import System.Environment (getArgs)
 
-readNames :: MigrationComp (AbsList AbsString)
-readNames =
-    let limited 0 = return Nil 
-        limited x = do
-            name <- readStr
-            end <- name === "end"
-            if end then do
-                return Nil
-            else do
-                rest <- limited (x-1)
-                return $ acons name rest
-    in limited 20
+checkPresence :: AbsList AbsString -> MigrationComp (AbsList AbsString)
+checkPresence names = 
+    let limited 0 _ = return Nil
+        limited n xs = do
+            x <- hd xs
+            case x of Nothing -> return Nil
+                      Just name -> do
+                        printStr $ "Is " +++ name +++ " present?"
+                        answer <- readStr
+                        present <- answer === "y"
+                        rest <- limited (n-1) (tl xs)
+                        if present then return $ acons name rest
+                                   else return rest
+    in limited 3 names
+
 
 registerComp :: MigrationComp ()
 registerComp = do
     printStr "Which class is this?"
     className <- readStr
-    printStr "Enter the present student names: ('end' to finish)"
-    names <- readNames
-    migrate "127.0.0.1"
+    printStr "Getting expected people..."
+    --migrate ("127.0.0.1", "8002")
+    --migrate ("127.0.0.1", "8001")
+    presence <- checkPresence ["Oscar", "Jamie", "John"]
+    printStr "Done. Reporting result."
+    migrate ("127.0.0.1", "8002")
     printStr $ "The people present in class " +++ className +++ " are:"
-    forEach (\name -> printStr name) names
+    forEach (\name -> printStr name) presence
     return ()
 
 main :: IO ()
-main = runMigrationComp registerComp
+main = do
+    --args <- getArgs
+    --let port = head args
+    runMigrationComp "8001" registerComp
